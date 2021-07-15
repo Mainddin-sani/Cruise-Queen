944efac1d4756cd9686d513663fdd7c365f8deb7
mainuddin sani <main12sani@gmail.com>
2021-07-15 23:29:18 +0600

    project done

--- app.js
+++ app.js
@@ -1,6 +1,60 @@
 var firstClassRent = parseInt(document.getElementById('first-class-cost').innerText);
 var economyClassRent = parseInt(document.getElementById('economy-class-cost').innerText);
 
+function getInputElement() {
+    var getInputElement = document.getElementById('flyFrom').value;
+    document.getElementById('flyingForm').innerText = getInputElement;
+
+    var getInputElement = document.getElementById('flyTo').value;
+    document.getElementById('flyingTo').innerText = getInputElement;
+
+    var getInputElement = document.getElementById('departure').value;
+    document.getElementById('departure-date').innerText = getInputElement;
+
+    var getInputElement = document.getElementById('return').value;
+    document.getElementById('return-date').innerText = getInputElement;
+
+
+    var firstClassCostValue = getInputHistory('first');
+    var economyClassCostValue = getInputHistory('economy');
+    var subtotal = firstClassCostValue * firstClassRent + economyClassCostValue * economyClassRent;
+    document.getElementById('subtotal').innerText = subtotal;
+    var CountryVat = 0.1 * subtotal;
+    document.getElementById('vat').innerText = CountryVat;
+    var grandTotal = subtotal + CountryVat;
+    document.getElementById('grand-total').innerText = grandTotal;
+
+    document.getElementById('subTotal').innerText = subtotal;
+
+    document.getElementById('Vat-chagre').innerText = CountryVat;
+
+    document.getElementById('Total-calc').innerText = grandTotal;
+
+    var getInputElement = document.getElementById('first-class-history').value;
+    document.getElementById('first-class').innerText = getInputElement;
+    var getInputElement = document.getElementById('economy-class-history').value;
+    document.getElementById('economy-class').innerText = getInputElement;
+
+    document.getElementById('first-class-Fixed-price').innerText = firstClassRent;
+    document.getElementById('economy-class-Fixed-price').innerText = economyClassRent;
+
+    document.getElementById('first-class-Fixed-price-total').innerText = firstClassCostValue * firstClassRent;
+
+    document.getElementById('economy-class-Fixed-price-total').innerText =  economyClassCostValue * economyClassRent;
+
+    
+    console.log(firstClassCostValue);
+
+    
+
+    
+}
+
+
+
+
+console.log(getInputElement);
+
 // get input value
 function getInputHistory(inputName) {
     var getInputName = document.getElementById(inputName + '-class-history');
@@ -24,6 +78,7 @@ function countHandler(isIncrement, inputName) {
     }
      // set input value
     document.getElementById(inputName + "-class-history").value = incrementValue;
+    console.log(incrementValue);
     calculateCost();
 }
 
